a2d339c45086cfc87502a0500e7d065da769a77b
nikola.golijanin <nikola.golijanin@vegait.rs>
2022-12-22 22:54:02 +0100

    Configuring a Logging Service

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace CompanyEmployees.Extensions
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions
 {
     public static class ServiceExtensions
     {
@@ -10,6 +13,9 @@
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
 
 
     }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,10 +1,14 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+"/nlog.config"));
 builder.Services.ConfigureCors();
+builder.Services.ConfigureLoggerService();
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -15,7 +19,7 @@ if (app.Environment.IsDevelopment())
 else
     app.UseHsts();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {

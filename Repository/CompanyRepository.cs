4ac7a48afc6c4c261c016a14623b72f457d684eb
nikola.golijanin <nikola.golijanin@vegait.rs>
2023-01-04 21:58:45 +0100

    Creating Resources

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -9,11 +9,21 @@ namespace Repository
         {
         }
 
+        public void CreateCompany(Company company)
+        {
+            Create(company);
+        }
+
         public IEnumerable<Company> GetAllCompanies(bool trackChanges)
         {
             return FindAll(trackChanges)
                     .OrderBy(c=>c.Name)
                     .ToList();
+        }
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges)
+        {
+            return FindByCondition(c => ids.Contains(c.Id),trackChanges);
         }
 
         public Company GetCompany(Guid companyId, bool trackChanges)

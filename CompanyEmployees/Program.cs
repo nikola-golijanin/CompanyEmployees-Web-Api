ca8058c062d1b95964cf32912ff68e1c49117c3b
nikola.golijanin <nikola.golijanin@vegait.rs>
2023-01-04 19:12:44 +0100

    Content Negotiation

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -14,7 +14,14 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddControllers().AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddControllers(config =>
+    {
+        config.RespectBrowserAcceptHeader = true;
+        config.ReturnHttpNotAcceptable = true;
+    })
+    .AddCustomCSVFormatter()
+    .AddXmlDataContractSerializerFormatters()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 
 var app = builder.Build();

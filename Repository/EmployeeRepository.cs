800801679ceeae133f61a861a11308519462df7c
nikola.golijanin <nikola.golijanin@vegait.rs>
2023-01-07 19:54:34 +0100

    Asynchronous Code

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
@@ -20,17 +21,18 @@ namespace Repository
             Delete(employee);
         }
 
-        public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges)
         {
-            return FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
-                            .SingleOrDefault();
+           return await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
+                .SingleOrDefaultAsync();
+
         }
 
-        public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges)
+        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges)
         {
-            return FindByCondition(e => e.CompanyId.Equals(companyId),trackChanges)
-                .OrderBy(e=>e.Name)
-                .ToList();
+            return await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                        .OrderBy(e => e.Name)
+                        .ToListAsync();
         }
     }
 }

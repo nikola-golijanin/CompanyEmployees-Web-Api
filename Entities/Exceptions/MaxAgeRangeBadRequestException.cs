523cb9b4c44974499e6e0272049b79e25bb7927f
Nikola Golijanin <nikola.golijanin@vegait.rs>
2023-01-09 16:31:35 +0100

    Filtering

---
+++ Entities/Exceptions/MaxAgeRangeBadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public class MaxAgeRangeBadRequestException : BadRequestException
+{
+    public MaxAgeRangeBadRequestException()
+        : base("Max age cant be less than min age.")
+    {
+    }
+}

552278194d671e6ac1774f6b6f27719135b010ee
Nikola Golijanin <nikola.golijanin@vegait.rs>
2023-01-09 16:48:28 +0100

    Searching

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -6,4 +6,5 @@ public class EmployeeParameters : RequestParameters
     public uint MaxAge { get; set; } = int.MaxValue;
 
     public bool ValidAgeRange => MaxAge > MinAge;
+    public string? SearchTerm { get; set; }
 }
